c9c1196426755afaa6adbfa77087fd78dce4d68b
mandie021-moEqdO <mandie021@outlook.com>
2021-10-05 21:12:20 -0400

    progress 10.5pm

--- Pybank/main.py
+++ Pybank/main.py
@@ -5,24 +5,45 @@ import csv
 #module for reading csv file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+
+
+
 #open file in read mode
-with open(csvpath, "r") as f:
+with open(csvpath, "r") as csvfile:
 
-#csvfile reader with delimiter
-    csv_reader = csv.reader(f, delimiter=",")
-    
+#csvfile reader with delimiter - delimiter did not work so using split in for loop
+    csv_reader = csv.reader(csvfile)
+    #ListofRows = list(csv_reader)
+
     #skipping the header
-    next(f)
-
+    next(csvfile)
+    #print(csv_reader)
     #making iteration for line count ---print outdented with the for statement to print complete total
     LineCount = 0
     TotalSum = 0
-    for line in f:
-        #print(line)
+    AvChange = 0
+    #Row = 0
+    
+    for line in csvfile:
+        line = line.split(',')
+        date = line[0]
+        profits = int(line[1])
         LineCount = LineCount + 1
-        TotalSum = sum + line[1]
-    print(LineCount)
-    print(TotalSum)
+        TotalSum = TotalSum + (profits)
+        CurLine = int(line[1])
+        
+
+
+    #AvChange = (CurLine + PrvLine)/2
+        print(CurLine)
+    
+    #print(LineCount)
+    #print(TotalSum)
+    #for row in CurLine:
+        #if CurLine != 
+
+
+       
 
             
     
