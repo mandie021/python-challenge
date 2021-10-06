d741e295b8c9ec8dea3083e7bacea6c2b3535b1c
mandie021-moEqdO <mandie021@outlook.com>
2021-10-06 07:11:04 -0400

    updated

--- Pybank/main.py
+++ Pybank/main.py
@@ -5,7 +5,9 @@ import csv
 #module for reading csv file
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-
+#make lists
+monthlist = []
+profitlist = []
 
 
 #open file in read mode
@@ -13,43 +15,43 @@ with open(csvpath, "r") as csvfile:
 
 #csvfile reader with delimiter - delimiter did not work so using split in for loop
     csv_reader = csv.reader(csvfile)
-    #ListofRows = list(csv_reader)
-
+    
     #skipping the header
     next(csvfile)
-    #print(csv_reader)
-    #making iteration for line count ---print outdented with the for statement to print complete total
-    LineCount = 0
-    TotalSum = 0
-    AvChange = 0
-    #Row = 0
-    
-    for line in csvfile:
-        line = line.split(',')
-        date = line[0]
-        profits = int(line[1])
-        LineCount = LineCount + 1
-        TotalSum = TotalSum + (profits)
-        CurLine = int(line[1])
-        
+    #print(csv_reader)  
 
+    for row in csv_reader:
+        monthlist.append(row[0])
+        profitlist.append(int(row[1]))
+    #print(monthlist)
+#getting the total months
+TotalMonths = len(monthlist)
 
-    #AvChange = (CurLine + PrvLine)/2
-        print(CurLine)
-    
-    #print(LineCount)
-    #print(TotalSum)
-    #for row in CurLine:
-        #if CurLine != 
+#net the profits
+TotalProfits = 0
 
+for i in profitlist:
+    TotalProfits = TotalProfits + i
+#print(TotalProfits)
 
-       
+#average change
+AvMonChangelist= []
+PrvMonAmount = 0
 
-            
-    
+for i in range(len(profitlist)):
+    if i == 0:
+        PrvMonAmount = profitlist[i]
+    else:
+        MonChange = profitlist[i] - PrvMonAmount
+        AvMonChangelist.append(MonChange)
+        PrvMonAmount = profitlist[i]
 
+#print monthly changes
+Duration = len(AvMonChangelist)
+total = sum(AvMonChangelist)
+ProfitAverage = total/Duration
+#print(ProfitAverage)
 
-    # render object to list 
-    #RowList = list(reader)
-    
+#finding min max and corrisponding month
+
 
